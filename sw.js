a48ba3841ac4b3bf92ec23c860521a53cbc37bf5
isak2405-lab <isak2405@gmail.com>
2026-09-16 21:27:32 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('noor-v3').then(c => c.addAll(['./','./index.html','./manifest.json'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
